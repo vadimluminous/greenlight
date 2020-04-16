a01865ea2e906ef57c560106ee74478f44bb2f9e
unknown <vadim@luminoustec.net>
2020-04-16 14:58:44 -0700

    pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ActionController::Base
+  layout 'application'
   def help
     # put any code here that you need 
     # (although for a static view you probably won't have any)
